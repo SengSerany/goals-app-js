e43a2cd44d5aebdf985185384870748d3211ae20
SengSerany <cranyseng@gmail.com>
2022-05-05 10:12:08 +0200

    set Router file and folder

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const portNumber = process.env.PORT_NUMBER || 5000;
 
 const app = express()
 
-app.get('/api/goals', (req, res) => {
-    res.status(200).json({message: 'Get Goals'});
-});
+app.use('/api/goals', require('./routes/goalRoutes'))
 
 app.listen(portNumber, () => {
     console.log(`Server is online on port ${portNumber}`)
