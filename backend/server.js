50de41102a2486e872198052a73575a74527613e
SengSerany <cranyseng@gmail.com>
2022-05-05 09:49:02 +0200

    set basic Express server + environment variable

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,9 @@
-console.log("hello world")
+const express = require('express');
+const dotenv = require('dotenv').config();
+const portNumber = process.env.PORT_NUMBER || 5000;
+
+const app = express()
+
+app.listen(portNumber, () => {
+    console.log(`Server is online on port ${portNumber}`)
+})
