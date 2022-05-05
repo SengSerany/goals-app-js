cfdd102e102f70c0d306c1d97cdeb6d9777017b8
SengSerany <cranyseng@gmail.com>
2022-05-05 11:17:48 +0200

    install express-async-handler for handling exceptions inside of async express routes and passing them to your express error handlers

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,14 +1,15 @@
+const asyncHandler = require('express-async-handler')
 // @desc     Get goals
 // @route    GET /api/goals
 // @access   Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
     res.status(200).json({message: 'Get Goals'});
-};
+});
 
 // @desc     Create goal
 // @route    POST /api/goals
 // @access   Private
-const createGoal = (req, res) => {
+const createGoal = asyncHandler(async (req, res) => {
     // Testing error handler with Postman adding in the body a key value named "text" with a random value
     if (!req.body.text) {
         res.status(400)
@@ -17,21 +18,21 @@ const createGoal = (req, res) => {
     }
 
     res.status(200).json({message: 'Create / Set Goal'});
-};
+});
 
 // @desc     update goals
 // @route    PATCH /api/goals/:id
 // @access   Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Update Goal ${req.params.id}`});
-};
+});
 
 // @desc     Delete goals
 // @route    DELETE /api/goals/:id
 // @access   Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({message: `Delete Goal ${req.params.id}`});
-};
+});
 
 module.exports = {
     getGoals,
