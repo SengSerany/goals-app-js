eae7b18727eb1f278e2ffaf0972c3b9c656d68cc
SengSerany <cranyseng@gmail.com>
2022-05-05 16:26:09 +0200

    update only the goal linked to the user

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,6 +1,8 @@
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
 
-const Goal = require('../models/goalModel')
+const Goal = require('../models/goalModel');
+const User = require('../models/userModel')
+
 
 // @desc     Get goals
 // @route    GET /api/goals
@@ -34,10 +36,25 @@ const createGoal = asyncHandler(async (req, res) => {
 const updateGoal = asyncHandler(async (req, res) => {
     const goal = await Goal.findById(req.params.id);
 
+    // Check for goal
     if (!goal) {
-        res.status(400)
-        throw new Error("Couldn't do the asked task")
-    }
+        res.status(400);
+        throw new Error("Goal not found");
+    };
+
+    const user = await User.findById(req.user.id);
+
+    // Check for user
+    if (!user) {
+        res.status(400);
+        throw new Error("User not found");
+    };
+
+    // Make sur the logged in user matches the goal user
+    if (goal.user.toString() !== user.id) {
+        res.status(401);
+        throw new Error("User not authorized");
+    };
 
     const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new: true})
     
