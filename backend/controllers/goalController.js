59c296752639c676808a2e0132eeb391375d5aa3
SengSerany <cranyseng@gmail.com>
2022-05-05 10:57:16 +0200

    set Error handling middleware (overwritting the default express Error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -9,7 +9,13 @@ const getGoals = (req, res) => {
 // @route    POST /api/goals
 // @access   Private
 const createGoal = (req, res) => {
-    console.log(req.body)
+    // Testing error handler with Postman adding in the body a key value named "text" with a random value
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add a text field')
+
+    }
+
     res.status(200).json({message: 'Create / Set Goal'});
 };
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware')
 const portNumber = process.env.PORT_NUMBER || 5000;
 
 const app = express()
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
 
 app.listen(portNumber, () => {
     console.log(`Server is online on port ${portNumber}`)
