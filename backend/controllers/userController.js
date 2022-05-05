e264d5ba6cb715c540227d93440c3c83d577036f
SengSerany <cranyseng@gmail.com>
2022-05-05 15:09:22 +0200

    set authentification user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -33,6 +33,7 @@ const registerUser = asyncHandler(async (req, res) => {
         password: hashedPassword
     });
 
+    // response
     if (user) {
         res.status(400).json({
             _id: user.id,
@@ -49,7 +50,22 @@ const registerUser = asyncHandler(async (req, res) => {
 // @route    POST /api/users/login
 // @access   Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({message: "Login user"})
+    const { email, password } = req.body;
+
+    //Check if user exist with email
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.status(200).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        });
+    } else {
+        res.status(400);
+        throw new Error('Your email or/and your password is wrong');
+    };
+
 })
 
 // @desc     Get user data
