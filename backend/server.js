f8bec4d4a6c3605f7227de6f56a0bc1acf2baa96
SengSerany <cranyseng@gmail.com>
2022-05-05 14:07:48 +0200

    setting primary User routes and controllers fonctions

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 require('dotenv').config();
-const colors = require('colors');
+require('colors');
 const { errorHandler } = require('./middleware/errorMiddleware')
 const portNumber = process.env.PORT_NUMBER || 5000;
 const connectDB = require('./config/db');
@@ -12,7 +12,8 @@ const app = express()
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', require('./routes/goalRoutes'))
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use('/api/users', require('./routes/userRoutes'));
 
 app.use(errorHandler)
 
