0154bec7b1d50264ee155d73f62542ec6e53259e
SengSerany <cranyseng@gmail.com>
2022-05-05 09:59:29 +0200

    set first route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,10 @@ const portNumber = process.env.PORT_NUMBER || 5000;
 
 const app = express()
 
+app.get('/api/goals', (req, res) => {
+    res.status(200).json({message: 'Get Goals'});
+});
+
 app.listen(portNumber, () => {
     console.log(`Server is online on port ${portNumber}`)
-})
+});
