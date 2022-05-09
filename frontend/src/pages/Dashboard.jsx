b0f5ae789cf9aebb270963b961eb82d726d7f47f
SengSerany <cranyseng@gmail.com>
2022-05-09 23:06:08 +0200

    fetch goals of user for dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,29 +1,60 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
-import GoalForm from "../components/GoalForm";
-
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import GoalForm from '../components/GoalForm'
+import Spinner from '../components/Spinner'
+import { getGoals, reset } from '../features/goals/goalSlice'
 
 function Dashboard() {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  const navigate = useNavigate();
-  const { user } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.auth)
+  // eslint-disable-next-line no-unused-vars
+  const { goals, isLoading, isError, message } = useSelector(
+    (state) => state.goals
+  )
+  
+  useEffect(() => {
+
+    if (isError) {
+      console.log(message)
+    }
+
+    if (!user) {
+      navigate('/login')
+    }
+    
+    return () => {
+      dispatch(reset())
+    }
+  }, [user, navigate, message, dispatch, isError]);
 
   useEffect(() => {
-    if (!user) {
-      navigate('/login');
-    };
-  }, [user, navigate]);
+    if (user) {
+      dispatch(getGoals())
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  if (isLoading) {
+    return <Spinner />
+  }
 
   return (
     <>
-      <section className="heading">
-        <h1>Welcome { user && user.name}</h1>
+      <section className='heading'>
+        <h1>Welcome {user && user.name}</h1>
         <p>Goals Dashboard</p>
       </section>
+
       <GoalForm />
-    </>
+
+      {goals.map( goal => {
+        return <p>{goal.text}</p>
+      })}
+      </>
   )
-};
+}
 
 export default Dashboard
