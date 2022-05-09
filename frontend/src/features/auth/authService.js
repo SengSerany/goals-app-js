81e2a89025de13bfb9f6ca93b0bf7e9b3ae1209c
SengSerany <cranyseng@gmail.com>
2022-05-09 13:10:26 +0200

    Login form working

--- frontend/src/features/auth/authService.js
+++ frontend/src/features/auth/authService.js
@@ -5,6 +5,7 @@ const API_URL = '/api/users/'
 // Register user
 const register = async (userData) => {
     const response = await axios.post(API_URL, userData);
+
     if(response.data) {
         localStorage.setItem('user', JSON.stringify(response.data))
     };
@@ -18,9 +19,21 @@ const logout = () => {
     localStorage.removeItem('user');
 }
 
+// Login user
+const login = async (userData) => {
+    const response = await axios.post(API_URL + "login", userData);
+    
+    if(response.data) {
+        localStorage.setItem('user', JSON.stringify(response.data))
+    };
+
+    return response.data;
+};
+
 const authService = {
     register,
-    logout
+    logout,
+    login
 };
 
 export default authService
